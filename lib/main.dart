95aaaf17d9e5b3f75dbfc60d28c30f81d3b2d28e
hammadx02 <hammadaali02@gmail.com>
2023-10-03 11:22:53 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: const PassStrengthChecker(),
